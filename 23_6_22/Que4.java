066f87eb18b90a360d34487d1351c789d2c58ab1
Bhatia Parth <77603364+Parth119@users.noreply.github.com>
2022-06-25 09:25:53 +0530

    Update Que4

--- 23_6_22/Que4.java
+++ 23_6_22/Que4.java
@@ -1,48 +1,72 @@
 /*
 * Que 4: create program to perform Addition,Subtration and multiplication using function interface with common function name ActionPerform.
 * */
-import java.util.*;
+import java.util.Scanner;
 import java.lang.*;
+
 @FunctionalInterface
-interface operation{
-    void ActionPerform(Integer n1,Integer n2);
+interface Addition{
+    int ActionPerform(Integer num1, Integer num2);
 }
 
-class Arithmatic implements operation{
+@FunctionalInterface
+interface Subtraction{
+    int ActionPerform(Integer num1, Integer num2);
+}
 
-    public void ActionPerform(Integer n1, Integer n2) {
-        while(true)
-        {
-            Scanner sc = new Scanner(System.in);
+@FunctionalInterface
+interface Multiplication{
+    int ActionPerform(Integer num1, Integer num2);
+}
+
+public class Que4 {
+    public static void main(String[] args) {
+
+//        Initialize variable
+        Integer num1;
+        Integer num2;
+        Integer ans;
+        Scanner sc = new Scanner(System.in);
+
+//        USer Entry
+        System.out.print("Enter First number :");
+        num1= sc.nextInt();
+
+        System.out.print("Enter Second number :");
+        num2 = sc.nextInt();
+
+
+//        Interface object
+        Addition a = (Integer n1, Integer n2) -> n1+n2;
+        Subtraction s = (Integer n1, Integer n2) -> n1-n2;
+        Multiplication m = (Integer n1, Integer n2) -> n1*n2;
+
+//        Switch Loop
+        while(true){
             System.out.println();
-            System.out.print("Enter 1 to Addition\n2 for Subtraction\n3 to Multipliation\n4 to exit\nEnter your choice:");
-            Integer num = sc.nextInt();
+            System.out.print("Enter \n1 for Addition\n2 for Subtraction\n3 for Multiplication\n4 to exit\nYour choice :");
+            Integer choice = sc.nextInt();
 
-            switch (num){
+            switch (choice)
+            {
                 case 1:
-                    System.out.println("Addition : "+n1+" + "+n2+" = "+(n1+n2));
+                    ans = a.ActionPerform(num1,num2);
+                    System.out.println("Addition :"+ans);
                     break;
                 case 2:
-                    System.out.println("Subtraction : "+n1+" - "+n2+" = "+(n1-n2));
+                    ans = s.ActionPerform(num1,num2);
+                    System.out.println("Subtraction :"+ans);
                     break;
                 case 3:
-                    System.out.println("Multiplication : "+n1+" * "+n2+" = "+(n1*n2));
+                    ans = m.ActionPerform(num1,num2);
+                    System.out.println("Multiplication :"+ans);
                     break;
                 case 4:
                     System.exit(1);
                 default:
-                    System.out.println("Invalid choice.");
+                    System.out.println("Invalid choice!");
             }
-
         }
 
-
-
-    }
-}
-public class Que4 {
-    public static void main(String[] args) {
-        Arithmatic a1 =new Arithmatic();
-        a1.ActionPerform(3,2);
     }
 }
