f793a234644e6e0796364a16ba8b1935bf339360
Bhatia Parth <77603364+Parth119@users.noreply.github.com>
2022-07-08 14:55:21 +0530

    CRUD in Memory
    Crud Operation in memory Using DTO and ResponseEntity

---
+++ 07_07_2022/dto-example-student/src/main/java/net/htrap/dtoexamplestudent/DTO/UserDTO.java
@@ -0,0 +1,11 @@
+package net.htrap.dtoexamplestudent.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private long id;
+    private String firstName;
+    private String email;
+}
