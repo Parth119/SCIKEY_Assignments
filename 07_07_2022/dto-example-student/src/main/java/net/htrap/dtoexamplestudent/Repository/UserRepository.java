eef1f16160dfbb6037bc4f19ec6c124dbc00c7e3
Bhatia Parth <77603364+Parth119@users.noreply.github.com>
2022-07-08 14:58:27 +0530

    Delete 07_07_2022 directory

--- 07_07_2022/dto-example-student/src/main/java/net/htrap/dtoexamplestudent/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package net.htrap.dtoexamplestudent.Repository;
-
-import net.htrap.dtoexamplestudent.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
